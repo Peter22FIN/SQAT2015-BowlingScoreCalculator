ccb1fdbb7d60e52f150db62e63f57d0202a8a601
somename <someemail>
2015-10-14 16:10:14 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -77,4 +77,30 @@ public class TestBowlingScoreCalculator {
 		bg.addFrame(frame1);
 		assertEquals(22, bg.score());
 	}
+	
+	@Test
+	public void testStrikeAndSpareReturn103() {
+		BowlingGame bg = new BowlingGame();
+		Frame frame0 = new Frame(10, 0);
+		Frame frame1 = new Frame(4, 6);
+		Frame frame2 = new Frame(7, 2);
+		Frame frame3 = new Frame(3, 6);
+		Frame frame4 = new Frame(4, 4);
+		Frame frame5 = new Frame(5, 3);
+		Frame frame6 = new Frame(3, 3);
+		Frame frame7 = new Frame(4, 5);
+		Frame frame8 = new Frame(8, 1);
+		Frame frame9 = new Frame(2, 6);
+		bg.addFrame(frame0);
+		bg.addFrame(frame1);
+		bg.addFrame(frame2);
+		bg.addFrame(frame3);
+		bg.addFrame(frame4);
+		bg.addFrame(frame5);
+		bg.addFrame(frame6);
+		bg.addFrame(frame7);
+		bg.addFrame(frame8);
+		bg.addFrame(frame9);
+		assertEquals(103, bg.score());
+	}
 }
