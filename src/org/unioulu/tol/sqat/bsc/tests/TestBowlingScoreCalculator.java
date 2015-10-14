1f7db28ddbfcf31c990cf15cfdaf3ab2bdfa47bb
somename <someemail>
2015-10-14 15:19:12 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -7,13 +7,6 @@ import org.unioulu.tol.sqat.bsc.BowlingGame;
 import org.unioulu.tol.sqat.bsc.Frame;
 
 public class TestBowlingScoreCalculator {
-
-	@Test
-	public void testFrame2and4() {
-		BowlingGame bg = new BowlingGame();
-		Frame frame = new Frame(2, 4);
-		bg.addFrame(frame);
-	}
 	
 	@Test
 	public void testFrameScore2and6Return9() {
